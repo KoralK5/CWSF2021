1e12c6f243e02aca6ea1322c90b1eeb76ef2f6e8
KoralK5 <62809012+KoralK5@users.noreply.github.com>
2021-05-19 16:33:54 -0400

    Update main.py

--- Library/Unoptimized/main.py
+++ Library/Unoptimized/main.py
@@ -1,38 +1,8 @@
 import numpy as np
 from time import time
 from CNN import *
+from train import *
 np.random.seed(1)
-
-def cross_entropy(p, q, epsilon=1e-12):
-	return -sum([p[i]*np.log2(q[i]+epsilon) for i in range(len(p))])
-
-
-def accuracy_eval(p, q):
-	return np.argmax(p) == np.argmax(q)
-
-
-def test(image, label, conv, maxp, acti):
-	out = conv.forward_prop(image)
-	out = maxp.forward_prop(out)
-	out = acti.forward_prop(out)
-
-	loss = cross_entropy(out, label)
-	accuracy = accuracy_eval(out, label)
-
-	return out, loss, accuracy
-
-
-def train(image, label, conv, maxp, acti, rate=0.005):
-	out, loss, acc = test(image, label, conv, maxp, acti)
-
-	gradient = np.zeros(label.shape)
-	gradient[np.argmax(label)] = -1 / out[np.argmax(label)]
-
-	back = acti.back_prop(gradient, rate)
-	back = maxp.back_prop(back)
-	back = conv.back_prop(back, rate)
-
-	return out, loss, acc
 
 inputs = np.random.rand(100,96,96*3)
 outputs = np.zeros((100,2))
@@ -46,17 +16,19 @@ for row in range(len(outputs)):
 
 start = time()
 l, a = [], []
-for row in range(len(outputs)):
-	output, loss, accuracy = train(inputs[row], outputs[row], conv, maxp, acti)
-	l.append(loss); a.append(accuracy)
+for epoch in range(3):
+	for row in range(len(outputs)):
+		output, loss, accuracy = train(inputs[row], outputs[row], conv, maxp, acti)
+		l.append(loss); a.append(accuracy)
 
-	print('Output   ➤ ', output)
-	print('Real     ➤ ', outputs[row])
-	print('Loss     ➤ ', loss)
-	print('Accuracy ➤ ', accuracy)
-	print('Time     ➤ ', f'{int(time() - start)}s\n')
+		print(f'\nITERATION {row+1} OF EPOCH {epoch+1}:\n')
+		print('Output    ➤ ', output)
+		print('Real      ➤ ', outputs[row])
+		print('Loss      ➤ ', loss)
+		print('Accuracy  ➤ ', accuracy)
+		print('Time      ➤ ', f'{int(time() - start)}s\n')
 
-print('\nTRAINING REPORT')
+print('\n\nTRAINING REPORT')
 print('Loss     ➤ ', sum(l)/len(l))
 print('Accuracy ➤ ', sum(a)/len(a))
 print('Duration ➤ ', '{0:.4f}'.format(time() - start), 'seconds')
