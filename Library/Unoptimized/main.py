43e2655c9cd8887c3192de035af5570bca9d8c75
KoralK5 <62809012+KoralK5@users.noreply.github.com>
2021-05-19 16:14:49 -0400

    training script implemented

--- Library/Unoptimized/main.py
+++ Library/Unoptimized/main.py
@@ -1,7 +1,7 @@
 import numpy as np
 from time import time
 from CNN import *
-
+np.random.seed(1)
 
 def cross_entropy(p, q, epsilon=1e-12):
 	return -sum([p[i]*np.log2(q[i]+epsilon) for i in range(len(p))])
@@ -11,10 +11,10 @@ def accuracy_eval(p, q):
 	return np.argmax(p) == np.argmax(q)
 
 
-def network(image, label, conv, maxp):
+def test(image, label, conv, maxp, acti):
 	out = conv.forward_prop(image)
 	out = maxp.forward_prop(out)
-	out = Softmax(np.prod(out.shape), label.size).forward_prop(out)
+	out = acti.forward_prop(out)
 
 	loss = cross_entropy(out, label)
 	accuracy = accuracy_eval(out, label)
@@ -22,41 +22,41 @@ def network(image, label, conv, maxp):
 	return out, loss, accuracy
 
 
-def train(image, label, conv, maxp, rate=0.1):
-	out, loss, acc = network(image, label, conv, maxp)
+def train(image, label, conv, maxp, acti, rate=0.1):
+	out, loss, acc = test(image, label, conv, maxp, acti)
 
-	gradient = np.zeros(1)
-	gradient[label] = -1/out[label]
-	
+	gradient = np.zeros(label.shape)
+	gradient[np.argmax(label)] = -1 / out[np.argmax(label)]
+
 	back = acti.back_prop(gradient, rate)
 	back = maxp.back_prop(back)
 	back = conv.back_prop(back, rate)
 
 	return out, loss, acc
 
+inputs = np.random.rand(100,96,96*3)
+outputs = np.zeros((100,2))
 
 conv = Conv(18,7)
 maxp = Maxpool(4)
-inputs = np.random.rand(100,96,96*3)
-outputs = np.zeros((100,2))
+acti = Softmax(22*70*18, outputs[0].size)
+
 for row in range(len(outputs)):
 	outputs[row][np.random.choice(len(outputs[row]))] = 1
 
-
 start = time()
-l, a, size = 0, 0, len(outputs)
-for row in range(size):
-	output, loss, accuracy = train(inputs[row], outputs[row], conv, maxp)
-	l += loss; a += accuracy
+l, a = [], []
+for row in range(len(outputs)):
+	output, loss, accuracy = train(inputs[row], outputs[row], conv, maxp, acti)
+	l.append(loss); a.append(accuracy)
 
 	print('Output   ➤ ', output)
 	print('Real     ➤ ', outputs[row])
 	print('Loss     ➤ ', loss)
 	print('Accuracy ➤ ', accuracy)
 	print('Time     ➤ ', f'{int(time() - start)}s\n')
-end = time()
 
-
-print('\nLoss     ➤ ', l/size)
-print('Accuracy ➤ ', a/size)
-print('Time     ➤ ', '{0:.4f}'.format(end - start), 'seconds')
+print('\nTRAINING REPORT')
+print('Loss     ➤ ', sum(l)/len(l))
+print('Accuracy ➤ ', sum(a)/len(a))
+print('Duration ➤ ', '{0:.4f}'.format(time() - start), 'seconds')
